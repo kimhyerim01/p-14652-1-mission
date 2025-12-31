a3c26a4f72cbb627c561cad7b9c7655d0be05f36
김혜림 <khrim981209@gmail.com>
2025-12-31 14:26:24 +0900

    0002

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
 }
 
 tasks.withType<Test> {
